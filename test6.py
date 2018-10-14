0ba9d90bd1fdf39e75047ba67352cc12b646742d
Zichen Guo <guozichen1996@gmail.com>
2018-10-14 23:28:19 +0800

    A collect parameters demo
    use collect parameters to print sth.

---
+++ test6.py
@@ -0,0 +1,6 @@
+def test(*param):
+	print('the lenth of the param is', len(param))
+	print('the first param is ', param[0])
+	print('the second param is ', param[1])
+
+test('asdf','aabb','ddddd',3,789)
