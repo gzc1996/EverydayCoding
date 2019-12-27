3c3c801319a9ad348f22cc5f5b05164b72371b3c
Scream Guo <43976297+gzc1996@users.noreply.github.com>
2019-12-27 23:16:30 +0800

    leetcode easy 234

---
+++ Palindrome_Linked_List.py
@@ -0,0 +1,48 @@
+'''
+Given a singly linked list, determine if it is a palindrome.
+
+Example 1:
+
+Input: 1->2
+Output: false
+Example 2:
+
+Input: 1->2->2->1
+Output: true
+'''
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution1:
+    def isPalindrome(self, head: ListNode) -> bool:
+        stack = []
+        
+        while head:
+            stack.append(head.val)
+            head = head.next
+        
+        if stack == stack[::-1]:
+            return True
+        else:
+            return False
+        
+
+class Solution2:
+    def isPalindrome(self, head):
+        rev = None
+        slow = fast = head
+        while fast and fast.next:
+            fast = fast.next.next
+            rev, rev.next, slow = slow, rev, slow.next
+        if fast:
+            slow = slow.next
+        while rev and rev.val == slow.val:
+            slow = slow.next
+            rev = rev.next
+        return not rev
+        
+        
